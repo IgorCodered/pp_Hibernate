1c70e1dc71bcc2dc6ffb367255682e861d682680
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2024-02-07 23:38:00 +0700

    createUsersTable us ok

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,33 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private final Connection dbconnection = Util.connection;
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try {
+            Statement statement = dbconnection.createStatement();
+            String SQL = "CREATE TABLE IF NOT EXISTS User " +
+                    "(id INT PRIMARY KEY, " +
+                    "name VARCHAR(100), " +
+                    "lastname VARCHAR(100), " +
+                    "age INT);";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,24 +1,27 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao userDAO = new UserDaoJDBCImpl();
     public void createUsersTable() {
-
+        userDAO.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDAO.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDAO.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        userDAO.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
@@ -26,6 +29,6 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-
+        userDAO.cleanUsersTable();
     }
 }
